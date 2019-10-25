947a8ac70d0d9a317198dd98166ae1ada9348651
CrankyBanana <50312888+CrankyBanana@users.noreply.github.com>
2019-10-26 00:49:49 +1100

    Cleaned some code

--- src/RPS/RPS.java
+++ src/RPS/RPS.java
@@ -13,17 +13,16 @@ class RPS {
     }
 
     private void run() {
-        Player p1 = new Player("Jaden");
-        Player p2 = new Player("Phong");
         List<Player> players = new ArrayList<Player>();
-        players.add(p1);
-        players.add(p2);
+        players.add(new Player("Jaden"));
+        players.add(new Player("Phong"));
         for (Player p : players) {
             play(p);
         }
-        System.out.println(whoWins(p1, p2));
+        System.out.println(whoWins(players.get(0), players.get(1)));
     }
 
+    // Retrieves user's decision
     private void play(Player p) {
         String value = in.nextLine("It's " + p.name + "'s turn What would you like to play? (r/p/s): ");
   
@@ -31,27 +30,32 @@ class RPS {
             case "r": System.out.println(p.name + " played rock!"); p.value = "r"; break;
             case "p": System.out.println(p.name + " played paper!"); p.value = "p"; break;
             case "s": System.out.println(p.name + " played scissors!"); p.value = "s"; break;
-            default: System.out.println("Error");
         }
     }
 
+    // Handles decision-making logic
     private String whoWins(Player p1, Player p2) {
         String winner = "";
         String hand = p1.value;
         String hand2 = p2.value;
-        
-        if (hand.equals("r") && hand2.equals("p")) { winner = p2.name + " wins!"; }
-        else if (hand.equals("r") && hand2.equals("s")) { winner = p1.name + " wins!"; }
-        else if (hand.equals("r") && hand2.equals("r")) { winner = "It's a tie!"; }
 
-        if (hand.equals("p") && hand2.equals("s")) { winner = p2.name + " wins!"; }
-        else if (hand.equals("p") && hand2.equals("r")) { winner = p1.name + " wins!"; }
-        else if (hand.equals("p") && hand2.equals("p")) { winner = "It's a tie!"; }
-
-        if (hand.equals("s") && hand2.equals("r")) { winner = p2.name + " wins!"; }
-        else if (hand.equals("s") && hand2.equals("p")) { winner = p1.name + " wins!"; }
-        else if (hand.equals("s") && hand2.equals("s")) { winner = "It's a tie!"; }
-
+        switch(hand) {
+            case "r":
+                if (hand2.equals("p")) { winner = p2.name + " wins!"; }
+                if (hand2.equals("s")) { winner = p1.name + " wins!"; }
+                if (hand2.equals("r")) { winner = "It's a tie!"; }
+                break;
+            case "p":
+                if (hand2.equals("s")) { winner = p2.name + " wins!"; }
+                if (hand2.equals("r")) { winner = p1.name + " wins!"; }
+                if (hand2.equals("p")) { winner = "It's a tie!"; }
+                break;
+            case "s":
+                if (hand2.equals("r")) { winner = p2.name + " wins!"; }
+                if (hand2.equals("p")) { winner = p1.name + " wins!"; }
+                if (hand2.equals("s")) { winner = "It's a tie!"; }
+                break;
+        }   
         return winner;
     }
 }
